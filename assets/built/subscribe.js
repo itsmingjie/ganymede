3d0e0c12817940866de53bd2b33f0d56269a9082
Mingjie Jiang <jiang@mingjie.info>
2019-12-28 17:17:48 -0500

    Code standard fix

--- assets/built/subscribe.js
+++ assets/built/subscribe.js
@@ -2,24 +2,30 @@
  * Subscribe by forwarding to Zapier
  */
 
+function isEmail(email) {
+  var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(email);
+}
+
 $("#subscribe__submit").click(() => {
   if (
-    $(".subscribe__input").eq(0).val() != "" &&
-    isEmail($(".subscribe__input").eq(1).val())
+    $(".subscribe__input")
+      .eq(0)
+      .val() !== "" &&
+    isEmail(
+      $(".subscribe__input")
+        .eq(1)
+        .val()
+    )
   ) {
     $.ajax({
       url: "https://hooks.zapier.com/hooks/catch/2085782/otichcc/",
       type: "post",
       data: $("#subscribe__form").serialize(),
-      success: function() {
+      success() {
         $("#subscribe__form").hide();
         $("#subscribe__title").text("Thanks for subscribing!");
       }
     });
   }
 });
-
-function isEmail(email) {
-  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  return regex.test(email);
-}
