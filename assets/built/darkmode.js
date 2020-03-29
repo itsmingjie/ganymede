85cea886a17742b6357fa1e875e17175fffb12cd
Mingjie Jiang <jiang@mingjie.info>
2020-03-29 15:19:27 -0400

    Update dark mode

--- assets/built/darkmode.js
+++ assets/built/darkmode.js
@@ -1,7 +1,50 @@
-function toggleTheme(isDark) {
-  if (isDark) {
-    document.documentElement.setAttribute('data-theme', 'dark')
-  } else {
-    document.documentElement.removeAttribute('data-theme')
+const STORAGE_KEY = 'user-color-scheme';
+const COLOR_MODE_KEY = '--color-mode';
+
+const toggle = document.querySelector('.theme__toggle');
+
+const getCSSCustomProp = propKey => {
+  let response = getComputedStyle(document.documentElement).getPropertyValue(propKey);
+
+  if (response.length) {
+    response = response.replace(/\"/g, '').trim();
   }
-}
+
+  return response;
+};
+
+const applySetting = passedSetting => {
+  let currentSetting = passedSetting || localStorage.getItem(STORAGE_KEY);
+
+  if (currentSetting) {
+    document.documentElement.setAttribute('data-user-color-scheme', currentSetting);
+  }
+};
+
+const toggleSetting = () => {
+  let currentSetting = localStorage.getItem(STORAGE_KEY);
+
+  switch (currentSetting) {
+    case null:
+      currentSetting = getCSSCustomProp(COLOR_MODE_KEY) === 'dark' ? 'light' : 'dark';
+      break;
+    case 'light':
+      currentSetting = 'dark';
+      break;
+    case 'dark':
+      currentSetting = 'light';
+      break;
+  }
+
+  localStorage.setItem(STORAGE_KEY, currentSetting);
+
+  return currentSetting;
+};
+
+toggle.addEventListener('click', evt => {
+  evt.preventDefault();
+
+  applySetting(toggleSetting());
+});
+
+applySetting();
